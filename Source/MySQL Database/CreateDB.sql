dfd59cf99d3e56793aa4d89ec3bf89e77bb4604d
GrenoZee <grenozee@telenet.be>
2017-10-23 13:21:27 +0200

    Added tables required by OAuth2 library

--- Source/MySQL Database/CreateDB.sql
+++ Source/MySQL Database/CreateDB.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Oct 12, 2017 at 05:53 PM
+-- Generation Time: Oct 23, 2017 at 01:17 PM
 -- Server version: 5.6.35
--- PHP Version: 5.5.38
+-- PHP Version: 7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -13,8 +13,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `faction_guard`
 --
-CREATE DATABASE IF NOT EXISTS `faction_guard` DEFAULT CHARACTER SET utf16 COLLATE utf16_unicode_ci;
-USE `faction_guard`;
 
 -- --------------------------------------------------------
 
@@ -55,6 +53,104 @@ CREATE TABLE `CommanderGroup` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `oauth_access_tokens`
+--
+
+CREATE TABLE `oauth_access_tokens` (
+  `access_token` varchar(40) COLLATE utf16_unicode_ci NOT NULL,
+  `client_id` varchar(80) COLLATE utf16_unicode_ci NOT NULL,
+  `user_id` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(4000) COLLATE utf16_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_authorization_codes`
+--
+
+CREATE TABLE `oauth_authorization_codes` (
+  `authorization_code` varchar(40) COLLATE utf16_unicode_ci NOT NULL,
+  `client_id` varchar(80) COLLATE utf16_unicode_ci NOT NULL,
+  `user_id` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `redirect_uri` varchar(2000) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(4000) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `id_token` varchar(1000) COLLATE utf16_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_clients`
+--
+
+CREATE TABLE `oauth_clients` (
+  `client_id` varchar(80) COLLATE utf16_unicode_ci NOT NULL,
+  `client_secret` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `redirect_uri` varchar(2000) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `grant_types` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `scope` varchar(4000) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `user_id` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_jwt`
+--
+
+CREATE TABLE `oauth_jwt` (
+  `client_id` varchar(80) COLLATE utf16_unicode_ci NOT NULL,
+  `subject` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `public_key` varchar(2000) COLLATE utf16_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_refresh_tokens`
+--
+
+CREATE TABLE `oauth_refresh_tokens` (
+  `refresh_token` varchar(40) COLLATE utf16_unicode_ci NOT NULL,
+  `client_id` varchar(80) COLLATE utf16_unicode_ci NOT NULL,
+  `user_id` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(4000) COLLATE utf16_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_scopes`
+--
+
+CREATE TABLE `oauth_scopes` (
+  `scope` varchar(80) COLLATE utf16_unicode_ci NOT NULL,
+  `is_default` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_users`
+--
+
+CREATE TABLE `oauth_users` (
+  `username` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `password` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `first_name` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `last_name` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `email` varchar(80) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `email_verified` tinyint(1) DEFAULT NULL,
+  `scope` varchar(4000) COLLATE utf16_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `User`
 --
 
@@ -90,6 +186,36 @@ ALTER TABLE `Commander`
 ALTER TABLE `CommanderGroup`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `IX_CommanderGroup_Name_Unique` (`Name`);
+
+--
+-- Indexes for table `oauth_access_tokens`
+--
+ALTER TABLE `oauth_access_tokens`
+  ADD PRIMARY KEY (`access_token`);
+
+--
+-- Indexes for table `oauth_authorization_codes`
+--
+ALTER TABLE `oauth_authorization_codes`
+  ADD PRIMARY KEY (`authorization_code`);
+
+--
+-- Indexes for table `oauth_clients`
+--
+ALTER TABLE `oauth_clients`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `oauth_refresh_tokens`
+--
+ALTER TABLE `oauth_refresh_tokens`
+  ADD PRIMARY KEY (`refresh_token`);
+
+--
+-- Indexes for table `oauth_scopes`
+--
+ALTER TABLE `oauth_scopes`
+  ADD PRIMARY KEY (`scope`);
 
 --
 -- Indexes for table `User`
